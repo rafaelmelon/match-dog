95b9bbd7a90e2dd7969ef0dfb955fb2640e8d300
Rafael Melón <info@rafaelmelon.es>
2017-05-10 00:39:12 +0200

    User render + Navbar - variables locales de usuario sesión y nuevo nabab bootstrap

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,20 @@
 /* jshint esversion:6 */
 
 const express = require('express');
+const passport = require('passport');
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 const router  = express.Router();
 
-router.get('/profile', (req, res, next) => {
-  res.render('user/profile');
+router.get('/profile', ensureLoggedIn('/login'), (req, res, next) => {
+  res.render('user/profile',{user : req.user});
 });
 
 router.get('/profile-edit', (req, res, next) => {
-  res.render('user/profile-edit');
+  res.render('user/profile-edit',{user : req.user});
 });
 
 router.get('/chat', (req, res, next) => {
-  res.render('user/chat');
+  res.render('user/chat',{user : req.user});
 });
 
 module.exports = router;
